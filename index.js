dfde64a4e9feed2916c0b52f07c0123f07e0a7d4
babu97 <benkaimugul@gmail.com>
2023-09-20 06:54:12 +0300

    finished functions

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+const  hqStreet = 42;
+
+return  Math.abs(pickupLocation- hqStreet)
+}
+
+function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+      const feet = blocks * 264;
+  
+    return feet;
+  }
+  function distanceTravelledInFeet( startBlock, endBlock){
+    const distanceInFeet = Math.abs(endBlock - startBlock) * 264;
+
+
+    return distanceInFeet;
+
+  }
+
+
+  function calculatesFarePrice(startBlock, endBlock) {
+    const distanceInFeet = Math.abs(endBlock - startBlock) * 264;
+  
+    if (distanceInFeet > 2500) {
+      return 'cannot travel that far';
+    } else if (distanceInFeet <= 400) {
+      
+      return 0;
+    } else if (distanceInFeet <= 2000) {
+    
+      return (distanceInFeet - 400) * 0.02;
+    } else {
+      return 25;
+    }
+  }
+  
